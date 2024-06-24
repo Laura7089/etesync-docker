9a6f80ed09ae0676727041f1fdc3963c439ae7ef
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-06-24 19:40:02 +0100

    fix: make static file generation non-interactive and overwrite by default

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -eu
 
 # adapted from the README at https://github.com/etesync/server/blob/386c01d89e8144e4b0609faa2054c5c83a761742/README.md
 ./manage.py migrate
-./manage.py collectstatic
+./manage.py collectstatic --no-input --clear
 
 uvicorn etebase_server.asgi:application --host 0.0.0.0 --port 8000 "$@"
